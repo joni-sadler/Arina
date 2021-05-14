b716955d12db9e8191399c0dbf5ae13dfde1b046
Joni Sadler <joni.anne.sadler@gmail.com>
2021-05-14 08:42:47 -0400

    Added GlobalStyles.js

---
+++ front-end/src/GlobalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+p {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 18px;
+}
+
+`;
